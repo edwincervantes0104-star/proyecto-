2a620b71987300753e35383723a752f0faf880e5
edwincervantes0104-star <edwincervantes0104@gmail.com@gmail.com>
2025-12-24 21:25:59 -0600

    corregi el error de recuperar contraseña :)

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'edwincervantes0104@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jrhivripybwmefmj'  # Contraseña de aplicación de Gmail
+app.config['MAIL_PASSWORD'] = 'whjmtcwuvnhxdvre'  # Contraseña de aplicación de Gmail
 app.config['MAIL_DEFAULT_SENDER'] = ('CECYTEM App', app.config['MAIL_USERNAME'])
 
 # ---------- Inicialización después de configurar ----------
