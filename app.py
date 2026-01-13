cbcc8ed04d005de558e7f7ac559ded7a7f96d165
aixa-source <aixa8868@gmail.com>
2026-01-12 19:21:20 -0600

    MENU ESTILOS MILDRED AZUL CARDENAS LOPEZ

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ serializer = URLSafeTimedSerializer(app.secret_key)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'usuario'
+app.config['MYSQL_DB'] = 'usuarios'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialize MySQL extension
